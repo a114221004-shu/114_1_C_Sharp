820b9d0d0e0f420dc2eb74911926a7dd36cd3ae8
a114221004-shu <a114221004@mail.shu.edu.tw>
2025-09-10 12:57:03 +0800

    114-09-10

---
+++ 114_09_10/HellowWorld/HellowWorld/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace HellowWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            checkedListBox1 = new CheckedListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("新細明體", 20F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label1.Location = new Point(328, 200);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 40);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(491, 317);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(180, 139);
+            checkedListBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(469, 106);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Try Ｍｅ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private CheckedListBox checkedListBox1;
+        private Button button1;
+    }
+}
