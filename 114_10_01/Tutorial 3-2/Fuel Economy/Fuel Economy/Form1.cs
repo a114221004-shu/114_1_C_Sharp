ba87654bc44aeebafd76cfeb658b4ee91919eeea
a114221004-shu <a114221004@mail.shu.edu.tw>
2025-10-01 13:01:03 +0800

    114_10_01

---
+++ 114_10_01/Tutorial 3-2/Fuel Economy/Fuel Economy/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fuel_Economy
+{
+    public partial class Form1 : Form
+    {
+        // 建構函式，初始化表單元件
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 當使用者按下「計算油耗」按鈕時執行
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double kilometers; // 宣告公里數變數（目前尚未使用）
+            double liters;
+            double kmpl;
+
+            kilometers = double.Parse(milesTextBox.Text);
+            liters = double.Parse(gallonsTextBox.Text);
+
+            kmpl = kilometers / liters;
+
+            mpgLabel.Text = kmpl.ToString("n2") + "公里/公升";
+        }
+
+        // 當使用者按下「離開」按鈕時執行
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
---
+++ 114_10_01/Tutorial_3_1/Tutorial_3_1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial_3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showDateButton_Click(object sender, EventArgs e)
+        {
+            string dayOfWeek = dayOfWeekTextBox.Text;
+            string month = dayOfMonthTextBox.Text;
+            string year = dayOfYearTextBox.Text;
+            string dayOfMonth = dayOfDateTextBox.Text;
+
+            dateOutputLabel.Text = " 中華民國 " + year + " 年 " + month + " 月 " + dayOfMonth + " 日 " + " ，星期 " + dayOfWeek;
+             
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            dayOfWeekTextBox.Text = "";
+            dayOfDateTextBox.Text = "";
+            dayOfMonthTextBox.Text = "";
+            dayOfYearTextBox.Text = "";
+            dateOutputLabel.Text = "";
+        }
+    }
+}
