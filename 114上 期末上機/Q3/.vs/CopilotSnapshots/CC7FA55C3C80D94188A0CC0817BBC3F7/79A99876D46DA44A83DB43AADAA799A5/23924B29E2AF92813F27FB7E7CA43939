fbe82b08e597ef29c07bc313130b7ba81664ab36
a114221004-shu <a114221004@mail.shu.edu.tw>
2025-12-24 12:26:55 +0800

    114-12-24

---
+++ 114上 期末上機/Q3/.vs/CopilotSnapshots/CC7FA55C3C80D94188A0CC0817BBC3F7/79A99876D46DA44A83DB43AADAA799A5/23924B29E2AF92813F27FB7E7CA43939
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Q3
+{
+    public partial class Form1 : Form
+    {
+        // Random instance for generating numbers
+        private readonly Random rng = new Random();
+        // flag to indicate whether a file has been successfully loaded
+        private bool fileLoaded = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 初始狀態若未開檔，不能按「產生號碼」
+            numberButton.Enabled = false;
+        }
+
+        private void opennumberButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "選擇檔案";
+                ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    listBox1.Items.Clear();
+                    try
+                    {
+                        string content;
+
+                        // First attempt: UTF8 with BOM detection
+                        using (var stream = ofd.OpenFile())
+                        using (var sr = new StreamReader(stream, Encoding.UTF8, detectEncodingFromByteOrderMarks: true))
+                        {
+                            content = sr.ReadToEnd();
+                        }
+
+                        // If replacement character found, try Big5 (codepage 950), then fallback to system default
+                        if (content.IndexOf('\uFFFD') >= 0)
+                        {
+                            bool success = false;
+                            try
+                            {
+                                using (var stream = ofd.OpenFile())
+                                using (var sr = new StreamReader(stream, Encoding.GetEncoding(950)))
+                                {
+                                    content = sr.ReadToEnd();
+                                    success = true;
+                                }
+                            }
+                            catch
+                            {
+                                // ignore and try default encoding
+                            }
+
+                            if (!success)
+                            {
+                                using (var stream = ofd.OpenFile())
+                                using (var sr = new StreamReader(stream, Encoding.Default))
+                                {
+                                    content = sr.ReadToEnd();
+                                }
+                            }
+                        }
+
+                        // Split lines and add to listBox
+                        using (var reader = new StringReader(content))
+                        {
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                listBox1.Items.Add(line);
+                            }
+                        }
+
+                        // 成功讀檔後啟用產生號碼按鈕
+                        fileLoaded = true;
+                        numberButton.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        fileLoaded = false;
+                        numberButton.Enabled = false;
+                        MessageBox.Show("讀檔時發生錯誤: " + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void numberButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!fileLoaded)
+                {
+                    throw new InvalidOperationException("尚未開啟檔案，請先開啟檔案後再產生號碼。");
+                }
+
+                var numbers = new HashSet<int>();
+                while (numbers.Count < 5)
+                {
+                    numbers.Add(rng.Next(1, 49));
+                }
+
+                var list = numbers.ToList();
+
+                // 將數字顯示在 label 上
+                number1Label.Text = list[0].ToString();
+                number2Label.Text = list[1].ToString();
+                number3Label.Text = list[2].ToString();
+                number4Label.Text = list[3].ToString();
+                number5Label.Text = list[4].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("無法產生號碼: " + ex.Message, "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ 114上 期末上機/Q3/Q3/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Q3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number1Label = new System.Windows.Forms.Label();
+            this.number5Label = new System.Windows.Forms.Label();
+            this.number4Label = new System.Windows.Forms.Label();
+            this.number3Label = new System.Windows.Forms.Label();
+            this.number2Label = new System.Windows.Forms.Label();
+            this.numberButton = new System.Windows.Forms.Button();
+            this.opennumberButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // number1Label
+            // 
+            this.number1Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number1Label.Font = new System.Drawing.Font("新細明體", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.number1Label.Location = new System.Drawing.Point(107, 85);
+            this.number1Label.Name = "number1Label";
+            this.number1Label.Size = new System.Drawing.Size(143, 90);
+            this.number1Label.TabIndex = 0;
+            this.number1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // number5Label
+            // 
+            this.number5Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number5Label.Font = new System.Drawing.Font("新細明體", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.number5Label.Location = new System.Drawing.Point(896, 85);
+            this.number5Label.Name = "number5Label";
+            this.number5Label.Size = new System.Drawing.Size(143, 90);
+            this.number5Label.TabIndex = 1;
+            this.number5Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // number4Label
+            // 
+            this.number4Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number4Label.Font = new System.Drawing.Font("新細明體", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.number4Label.Location = new System.Drawing.Point(706, 85);
+            this.number4Label.Name = "number4Label";
+            this.number4Label.Size = new System.Drawing.Size(143, 90);
+            this.number4Label.TabIndex = 2;
+            this.number4Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // number3Label
+            // 
+            this.number3Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number3Label.Font = new System.Drawing.Font("新細明體", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.number3Label.Location = new System.Drawing.Point(504, 85);
+            this.number3Label.Name = "number3Label";
+            this.number3Label.Size = new System.Drawing.Size(143, 90);
+            this.number3Label.TabIndex = 3;
+            this.number3Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // number2Label
+            // 
+            this.number2Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number2Label.Font = new System.Drawing.Font("新細明體", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.number2Label.Location = new System.Drawing.Point(307, 85);
+            this.number2Label.Name = "number2Label";
+            this.number2Label.Size = new System.Drawing.Size(143, 90);
+            this.number2Label.TabIndex = 4;
+            this.number2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numberButton
+            // 
+            this.numberButton.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.numberButton.Location = new System.Drawing.Point(55, 227);
+            this.numberButton.Name = "numberButton";
+            this.numberButton.Size = new System.Drawing.Size(232, 96);
+            this.numberButton.TabIndex = 5;
+            this.numberButton.Text = "產生號碼";
+            this.numberButton.UseVisualStyleBackColor = true;
+            this.numberButton.Click += new System.EventHandler(this.numberButton_Click);
+            // 
+            // opennumberButton
+            // 
+            this.opennumberButton.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.opennumberButton.Location = new System.Drawing.Point(471, 227);
+            this.opennumberButton.Name = "opennumberButton";
+            this.opennumberButton.Size = new System.Drawing.Size(232, 96);
+            this.opennumberButton.TabIndex = 6;
+            this.opennumberButton.Text = "開獎號碼";
+            this.opennumberButton.UseVisualStyleBackColor = true;
+            this.opennumberButton.Click += new System.EventHandler(this.opennumberButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.exitButton.Location = new System.Drawing.Point(909, 227);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(232, 96);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 36;
+            this.listBox1.Location = new System.Drawing.Point(55, 369);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(609, 364);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 850);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.opennumberButton);
+            this.Controls.Add(this.numberButton);
+            this.Controls.Add(this.number2Label);
+            this.Controls.Add(this.number3Label);
+            this.Controls.Add(this.number4Label);
+            this.Controls.Add(this.number5Label);
+            this.Controls.Add(this.number1Label);
+            this.Name = "Form1";
+            this.Text = "樂透號碼產生器";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label number1Label;
+        private System.Windows.Forms.Label number5Label;
+        private System.Windows.Forms.Label number4Label;
+        private System.Windows.Forms.Label number3Label;
+        private System.Windows.Forms.Label number2Label;
+        private System.Windows.Forms.Button numberButton;
+        private System.Windows.Forms.Button opennumberButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
